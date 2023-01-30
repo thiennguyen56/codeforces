6b1f5421b52f0c7517d73dc23d9aac94dd4cbe24
thiennd <ducthien562000@gmail.com>
2023-01-30 10:49:32 +0700

    723D update solution for testcase 3

---
+++ 161A/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int n, m, x, y;
+int a[100005];
+int b[100005];
+
+int main() {
+  cin >> n >> m >> x >> y;
+  for (int i = 1; i <= n; i++) {
+    cin >> a[i];
+  }
+  for (int i = 1; i <= n; i++) {
+    cin >> b[i];
+  }
+}
--- D/723D/main.cpp
+++ D/723D/main.cpp
@@ -1,36 +1,34 @@
+#include <algorithm>
 #include <iostream>
+#include <string>
 #include <vector>
 
 using namespace std;
 
-const int MAX_N = 50;
-int n, m, k;
-bool visited[MAX_N][MAX_N];
-string a[MAX_N];
-int lakes = 0;
-int transformed = 0;
+const int MAX_N = 53;
+int n, m, k, cnt = 0;
+string a[MAX_N], tmp[MAX_N];
 int dx[4] = {1, -1, 0, 0};
 int dy[4] = {0, 0, 1, -1};
 
-void init() {
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < m; j++) {
-      visited[i][j] = false;
-    }
-  }
-}
+struct element {
+  int square;
+  int x, y;
+};
+
+vector<element> save;
+
+bool cmp(const element &a, const element &b) { return (a.square < b.square); }
+
 void DFS(pair<int, int> start) {
   int x = start.first;
   int y = start.second;
-  visited[x][y] = true;
-  if (lakes > k) {
-    a[x][y] = '*';
-  }
+  cnt++;
+  a[x][y] = '*';
   for (int i = 0; i < 4; i++) {
     int xx = x + dx[i];
     int yy = y + dy[i];
-    if (xx >= 1 && xx < n - 1 && yy >= 1 && yy < m - 1 &&
-        visited[xx][yy] == false && a[xx][yy] == '.') {
+    if (xx >= 0 && xx < n && yy >= 0 && yy < m && a[xx][yy] == '.') {
       DFS(make_pair(xx, yy));
     }
   }
@@ -40,20 +38,34 @@ void solve() {
   cin >> n >> m >> k;
   for (int i = 0; i < n; i++) {
     cin >> a[i];
+    tmp[i] = a[i];
   }
-  init();
-  for (int i = 1; i < n - 1; i++) {
-    for (int j = 1; j < m - 1; j++) {
-      if (a[i][j] == '.' && visited[i][j] == false) {
-        lakes++;
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      if ((i == 0 || i == n - 1 || j == 0 || j == m - 1) && a[i][j] == '.') {
         DFS(make_pair(i, j));
-        if (lakes > k) {
-          transformed++;
-        }
       }
     }
   }
-  cout << transformed << endl;
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      if (a[i][j] == '.') {
+        cnt = 0;
+        DFS(make_pair(i, j));
+        save.push_back((element){cnt, i, j});
+      }
+    }
+  }
+  for (int i = 0; i < n; i++) {
+    a[i] = tmp[i];
+  }
+  sort(save.begin(), save.end(), cmp);
+  int res = 0;
+  for (int i = 0; i < save.size() - k; i++) {
+    res += save[i].square;
+    DFS(make_pair(save[i].x, save[i].y));
+  }
+  cout << res << endl;
   for (int i = 0; i < n; i++) {
     cout << a[i] << endl;
   }
